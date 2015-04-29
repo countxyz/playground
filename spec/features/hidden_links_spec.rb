8523e24ae9394119298b0bfe5bdd4176b90d39c4
count_xyz <skeptoid@gmail.com>
2015-04-29 08:59:22 -0400

    Ensure appropriate links are shown on sign in page.

--- spec/features/hidden_links_spec.rb
+++ spec/features/hidden_links_spec.rb
@@ -5,8 +5,24 @@ RSpec.feature 'hidden links' do
   context 'user without account' do
     before { visit '/' }
 
+    scenario "able to see 'Sign Up' link" do
+      assert_link_for 'Sign Up'
+    end
+
+    scenario "unable to see 'Sign In' link" do
+      assert_no_link_for 'Sign In'
+    end
+
     scenario "unable to see 'Profile' link" do
       assert_no_link_for 'Profile'
+    end
+
+    scenario "unable to see 'Accounts' link" do
+      assert_no_link_for 'Accounts'
+    end
+
+    scenario "unable to see 'Contacts' link" do
+      assert_no_link_for 'Contacts'
     end
 
     scenario "unable to see 'Sign Out' link" do
